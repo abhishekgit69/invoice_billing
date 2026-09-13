d2efb524dde8f618e705aa7ebfac87f38db3c6b7
ABHISHEK SHARMA <sampark.abhisheksharma@gmail.com>
2026-09-13 11:59:46 +0530

    Add safe environment template for production build

---
+++ src/environments/environment.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  production: true,
+  supabaseUrl: 'https://ricfzirhkrbfdwumrioe.supabase.co',
+  publishableKey: 'sb_publishable_9INvHXmFzDIBs16JP9HvPA_2a9hpyj3',
+};
