1eb4691e4f43169dd2030830cf9ec17b415513db
ABHISHEK SHARMA <sampark.abhisheksharma@gmail.com>
2026-09-15 15:18:57 +0530

    dployment correction

---
+++ src/environments/environment.development.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  production: false,
+  supabaseUrl: 'https://ricfzirhkrbfdwumrioe.supabase.co',
+  publishableKey: 'sb_publishable_9INvHXmFzDIBs16JP9HvPA_2a9hpyj3',
+};
